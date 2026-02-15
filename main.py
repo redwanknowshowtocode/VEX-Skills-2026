2ce6562c6c8a34e4f02fc56d6ce91c62d0fa2fbc
red <redwanrokan2010@gmail.com>
2026-02-14 21:25:24 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       mhpil                                                        #
+# 	Created:      2/14/2026, 9:18:21 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+brain = Brain()
+
+def autonomous():
+    brain.screen.clear_screen()
+    brain.screen.print("autonomous code")
+    # place automonous code here
+
+def user_control():
+    brain.screen.clear_screen()
+    brain.screen.print("driver control")
+    # place driver control in this while loop
+    while True:
+        wait(20, MSEC)
+
+# create competition instance
+comp = Competition(user_control, autonomous)
+
+# actions to do when the program starts
+brain.screen.clear_screen()
