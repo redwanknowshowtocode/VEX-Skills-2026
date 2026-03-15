2cfd7032dc9531b4bf5f09903da6b5b2f670d486
Adriannapa21-sys <Adriann40@nycstudents.net>
2026-03-15 17:43:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,23 +18,18 @@ controller = Controller()
 
 #these are the motor for the drive train, you can change the ports and gear settings as needed but the default gear setting is 18:1 and the default direction is forward (false)
 #SmartDrive will be set up later
-motorL1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-motorL2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
-motorR3 = Motor(Ports.PORT11, GearSetting.RATIO_18_1, True)
-motorR4 = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True)
-left_motor = MotorGroup(motorL1, motorL2,)
-right_motor = MotorGroup(motorR3, motorR4, )
-Base = DriveTrain(left_motor, right_motor) #sets the drive train to use the four motors
-
-
+motorL1 = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
+motorL2 = Motor(Ports.PORT12, GearSetting.RATIO_18_1, False)
+motorR3 = Motor(Ports.PORT19, GearSetting.RATIO_18_1, True)
+motorR4 = Motor(Ports.PORT20, GearSetting.RATIO_18_1, True)
 #odometry variables
 x = 0.0
 y = 0.0
 theta = 0.0 #measured in radians
 wheel_radius = 2 #in inches
 circumference = 2 * math.pi * wheel_radius
-IMU = Inertial(Ports.PORT20) #this is the inertial sensor, it will be used to get the robot's heading
-rotation_sensor = Rotation(Ports.PORT19) #this is the rotation sensor, it will be used to get the distance traveled by the robot
+IMU = Inertial(Ports.PORT6) #this is the inertial sensor, it will be used to get the robot's heading
+rotation_sensor = Rotation(Ports.PORT18) #this is the rotation sensor, it will be used to get the distance traveled by the robot
 
 
 
@@ -59,10 +54,45 @@ def update_position():
     x += dx
     y += dy
 
+def turn_to_angle(target_angle):
+    current_angle = IMU.heading
+    error = target_angle - current_angle
+    while abs(error) > 2:  # Allowable error of 2 degree
+        turn_speed = max(min(error * 0.5, 100), -100)  # Proportional control
+        motorL1.spin(FORWARD, turn_speed, PERCENT)
+        motorL2.spin(FORWARD, turn_speed, PERCENT)
+        motorR3.spin(FORWARD, -turn_speed, PERCENT)
+        motorR4.spin(FORWARD, -turn_speed, PERCENT)
+        wait(20, MSEC)
+        current_angle = IMU.heading
+        error = target_angle - current_angle
+
+def drive_straight(distance): 
+    rotation_sensor.reset_position()
+    IMU.reset_heading()
+    target_distance = distance
+
+    deadband = 1 #to keep the heading straight and locked while it drives straight, we have a deadband
+    while True:
+        if abs(IMU.heading()) < deadband:
+
+
+
+        
+
+def drive_to_point(target_x, target_y):
+    pass
+
 def autonomous():
     brain.screen.clear_screen()
     brain.screen.print("autonomous code")
-   
+    origin = (0, 0)
+
+    #a standard vex field is 12ft by 12ft, which is 144 inches by 144 inches, so the max distance the robot can travel in one direction is 144 inches but we are positioned at the bottom middle so we account for that value being at (72,0)
+    top_right_corner = (72, 144 )
+    top_left_corner = (-72, 144)
+    bottom_left_corner = (-72, 0)
+    bottom_right_corner = (72, 0)
 
 def user_control():
     brain.screen.clear_screen()
@@ -103,4 +133,5 @@ comp = Competition(user_control, autonomous)
 brain.screen.clear_screen()
 
 # run the drive task as a separate thread so that it can run at the same time as the user control and autonomous functions
-task1 = Thread(drive_task)
+#task1 = Thread(drive_task)
+task2 = Thread(drive_straight(10))
